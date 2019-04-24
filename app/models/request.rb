70772e7d65587b6e2f82a78fe64943e8eb364a5b
solve0919 <saltbitcoin@gmail.com>
2019-04-24 18:53:23 +0900

    enum_add

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,6 @@
 class Request < ApplicationRecord
   belongs_to :order
   belongs_to :contracrtor , optional: true
+
+  enum status: {request:0,consultation:1,orders:2,work:3, shipping:4,completion:5}
 end
