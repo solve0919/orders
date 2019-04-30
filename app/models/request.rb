51cba176a0b4d3f8e596f4436860a72bb40ab8a4
solve0919 <saltbitcoin@gmail.com>
2019-04-30 13:00:17 +0900

    fin_contoroller_to_model

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,6 +4,6 @@ class Request < ApplicationRecord
   enum status: {request:0,consultation:1,orders:2,work:3, shipping:4,completion:5}
 
   def can_access?(user)
-    user.id == self.contractor_id ||  user.id == self.order_id
+    user.id == self.contractor.user_id ||  user.id == self.order.user_id
   end
 end
