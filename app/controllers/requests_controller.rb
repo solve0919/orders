e7cb888368432d6d00620dbdda66cb3d040a6ac2
solve0919 <saltbitcoin@gmail.com>
2019-04-30 15:42:10 +0900

    fin_user_to_current_user

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -76,8 +76,7 @@ class RequestsController < ApplicationController
     def ensure_correct_user 
       @order = @request.order
       @contractor = @request.contractor
-      
-      unless @request.can_access?(current_user)
+      unless @request.can_access?(user)
         flash[:notice] = '権限がありません'
         redirect_to('/requests')
       end
--- app/models/request.rb
+++ app/models/request.rb
@@ -3,7 +3,7 @@ class Request < ApplicationRecord
   belongs_to :contractor , optional: true
   enum status: {request:0,consultation:1,orders:2,work:3, shipping:4,completion:5}
 
-  def can_access?(currenr_user)
-    currenr_user.id == self.contractor.user_id ||  currenr_user.id == self.order.user_id
+  def can_access?(user)
+    user.id == self.contractor.user_id ||  user.id == self.order.user_id
   end
 end
