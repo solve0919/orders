fd587a9f136c2850737437ddf07a76019eb0fd4e
solve0919 <saltbitcoin@gmail.com>
2019-05-02 14:46:36 +0900

    fin_until#15

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,7 @@
 class Request < ApplicationRecord
   belongs_to :order
   belongs_to :contractor , optional: true
-  enum status: {request:0 ,consultation:1 ,orders:2 ,work:3 ,shipping:4 ,completion:5 }
+  enum status: { request:0, consultation:1, orders:2, work:3, shipping:4 ,completion:5}
 
   def can_access?(user)
     user.id == self.contractor.user_id ||  user.id == self.order.user_id
