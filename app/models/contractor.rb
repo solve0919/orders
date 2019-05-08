75b02f38b28472c964be803bbc281ff771bb8b2d
solve0919 <saltbitcoin@gmail.com>
2019-05-08 13:47:38 +0900

    little_changed

--- app/models/contractor.rb
+++ app/models/contractor.rb
@@ -2,8 +2,8 @@ class Contractor < ApplicationRecord
   belongs_to :user
   has_many :requests
   validates :description, length: { in: 10..400 }
-  def login_user?(user)
-    #@contractor.user.id ==  current_user.id
+  def myself?(user)
+    #@current_user.id == contractor.user.id 
     user.id == self.user.id
   end
 end
