1bec1aad835e3a0dd730c8496c3c14627097de74
solve0919 <saltbitcoin@gmail.com>
2019-05-11 18:37:13 +0900

    add.contracrtor_column_category

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_101432) do
+ActiveRecord::Schema.define(version: 2019_05_11_093402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.integer "contractor_id"
+    t.integer "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contractor_categories", force: :cascade do |t|
+    t.integer "contractor_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "contractors", force: :cascade do |t|
     t.integer "user_id"
@@ -26,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_101432) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.integer "category_id"
   end
 
   create_table "images", force: :cascade do |t|
