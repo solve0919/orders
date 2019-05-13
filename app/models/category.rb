f15109fcd4def33d67e1e10344753492c6622106
solve0919 <saltbitcoin@gmail.com>
2019-05-13 17:05:36 +0900

    show_change

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,13 +1,4 @@
 class Category < ApplicationRecord
   has_many :contractor_category
-  has_many :contractor through: :contractor_category
-  enum name: {
-    beer:0 ,
-    alcohol:1 ,
-    drinking:2 ,
-    clothing:3 ,
-    accessories:4 ,
-    foods:5 ,
-    other:6 ,
-  }
+  has_many :contractor, through: :contractor_category
 end
