315c99bad4d387843581031aa62e15dc087539db
solve0919 <saltbitcoin@gmail.com>
2019-05-21 14:46:33 +0900

    change become motion

--- app/models/contractor.rb
+++ app/models/contractor.rb
@@ -2,8 +2,8 @@ class Contractor < ApplicationRecord
   belongs_to :user
   has_many :requests
   has_many :contractor_categories
-  has_many :categories, through: :contractor_category
-  accepts_nested_attributes_for :contractor_category
+  has_many :categories, through: :contractor_categories
+  accepts_nested_attributes_for :contractor_categories
 
   validates :description, length: { in: 10..400 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_18_063617) do
+ActiveRecord::Schema.define(version: 2019_05_18_102845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,8 @@ ActiveRecord::Schema.define(version: 2019_05_18_063617) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "judge_order"
+    t.boolean "judge_contractor"
   end
 
   create_table "users", force: :cascade do |t|
