cf47ebb43efa42a9b05143dab9414a1b27eba0c8
solve0919 <saltbitcoin@gmail.com>
2019-05-29 21:45:48 +0900

    aws

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   default from:     "メールテスト運営局",
           bcc:      "saltynem99@gmail.com",
+          reply_to: "sample+reply@gmail.com"
   layout 'mailer'
+  # layout 'mailer'
 end
